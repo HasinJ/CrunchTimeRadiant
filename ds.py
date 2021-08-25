c6211300ef07c3d5e5f86d5cae59601017c858bc
HasinJ <hasinchoudhuryj@gmail.com>
2021-08-25 01:27:01 -0400

    prints nodes

--- ds.py
+++ ds.py
@@ -2,6 +2,7 @@
 ## example Linked List:
 ## Last -> 3rd -> 2nd -> 1st
 ## Ending signified if 1st is the same as the NEW last
+##
 
 class Node:
     def __init__(self, PCNumber):
@@ -19,7 +20,24 @@ class LinkedList:
             node.count = 1
             self.head = node
             self.end = node
+            return
 
         node.count = self.head.count + 1
         node.next = self.head
         self.head = node
+
+    #def __repr__(self):
+       #pass
+
+    def __str__(self):
+        current = self.head
+        while current is not None:
+            print(f"{current.count} : {current.PC}")
+            current=current.next
+        return ""
+
+
+#LLtest = LinkedList()
+#LLtest.insertBeginning(Node("344754"))
+#LLtest.insertBeginning(Node("15124124"))
+#print(LLtest)
