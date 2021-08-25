b047b3da7d5ff8e4e902aa73a7f3e87dcdfb79c0
HasinJ <hasinchoudhuryj@gmail.com>
2021-08-25 12:34:32 -0400

    gets header

--- src/Scrapers/MenuMix/MenuMixScraper.py
+++ src/Scrapers/MenuMix/MenuMixScraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import csv
 import sys
-from os import path
+from pathlib import Path
 
 
 class MenuMixScrape():
@@ -15,16 +15,27 @@ class MenuMixScrape():
         self.data = []
         self.columns = []
 
-    def scrape(self, html):
+    def checkFolders(self):
+        pass
+
+    def scrapePC(self, html, PC):
         soup = BeautifulSoup(html, 'html.parser')
         self.data = []
         #results = soup.findAll("div", {"ces-selenium-id" : "component"})
 
-        
+        header = soup.findAll("span", {"class" : "x-column-header-text-inner"})
+        header = list(map(lambda element: element.string.strip(), header))
+        print(list(header))
+
+        rows = soup.findAll("tr", {"role" : "row"})
+        print(len(rows))
+
+
+
 
 if __name__ == "__main__":
     task = MenuMixScrape()
     f = open(fr'C:\Users\Hasin\Desktop\Projects\CrunchTimeRadiant\src\Scrapers\MenuMix\html_tests\MenuMix.html','rb') # 'rb' stands for read-binary, write-binary needs chmoding, this also needs to be changed for Selenium (needs to have date)
     content = f.read()
-    task.scrape(content)
+    task.scrapePC(content,"9999")
     f.close()
